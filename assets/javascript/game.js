8963d75fa70f4924d115e8ff5d316da9b96dddce
christopherdonner <christopher.donner@gmail.com>
2018-11-26 08:02:11 -0500

    working game engine

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,13 @@
 
 var userInput
-var secretWord
+var secretWord=[]
 var wordsList=[word1, word2, word3]
 var word1=["t","e","s","t"]
 var word2=[]
 var word3=[]
+var gameBoardtext = document.getElementById("blankWord");
+
+
 
 
 
@@ -23,10 +26,18 @@ var word3=[]
     for (var i=0; i<word1.length; i++){
         //console.log(word1[i]);
         if (userInput===word1[i]){
-            alert(`${word1[i]} is correct!`)
+            secretWord[i]=word1[i]
+            }
+            //alert(`${word1[i]} is correct!`)
         }
+    
+        for (var j=0; j<word1.length; j++){
+            secretWord.push("_")
+        }
+
+    
+    gameBoardtext.textContent=secretWord
     }
-}
 
 /*captureInput();
 checkValue();
