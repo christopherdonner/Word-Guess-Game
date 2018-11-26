c9e87df1250b5da1cf357b15eeccc44bed05abc9
Christopher Donner <christopher.donner@gmail.com>
2018-11-26 16:17:01 -0500

    simpsons theme applied

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,24 @@
 var userInput
 var secretWord=[]
-var word1=["t","e","s","t"]
-var word2=["c","h","r","i","s"]
-var word3=["d","o","n","n","e","r"]
-var wordsList=[word1, word2, word3]
+var word1=["m","a","r","g","e"]
+var word2=["b","a","r","t"]
+var word3=["h","o","m","e","r"]
+var word4=["l","i","s","a"]
+var word5=["m","i","l","l","h","o","u","s","e"]
+var wordsList=[word1, word2, word3, word4,word5]
 var gameBoardtext = document.getElementById("blankWord");
 var currentWord=[]
 var wordPickerInt
+var success
+var wordString="testWord"
+var wordArray=[]
+
+function arrayifyWords(){
+for (i=0; i<wordString.length; i++){
+    wordArray.push(wordString[i]);
+}
+console.log(wordArray)
+}
 
 wordPickerInt=Math.floor(Math.random() * wordsList.length);
 console.log(wordPickerInt);
@@ -35,6 +47,10 @@ gameBoardtext.textContent=secretWord
         //console.log(word1[i]);
         if (userInput===currentWord[i]){
             secretWord[i]=currentWord[i]
+            success++;
+                if (success===currentWord.length){
+                    alert("YOU DID IT!")
+                }
             }
             //alert(`${word1[i]} is correct!`)
         }
